6d97c700b5378e128d30faea32eb3411d69e36b2
Kamil Czyżnielewski <kamil.czyznielewski@gmail.com>
2022-04-20 19:43:45 +0200

    Reverting bank-provider changes (#8)

---
+++ bank-provider/src/test/kotlin/pl/sii/bank/bankprovider/BankProviderApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.sii.bank.bankprovider
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankProviderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
